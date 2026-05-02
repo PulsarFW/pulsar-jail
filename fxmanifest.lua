2da5400ddb36361bbb7806db9f38e7eee0d7013c
BaskinB <54458253+BaskinB@users.noreply.github.com>
2026-05-02 15:00:49 -0500

    feat(server): added version checking.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,18 +1,19 @@
 fx_version("cerulean")
 game("gta5")
 lua54("yes")
+version("1.0.0")
 
 client_script("@pulsar-core/exports/cl_error.lua")
 client_script("@pulsar-pwnzor/client/check.lua")
 
 server_scripts({
-	"server/**/*.lua",
+  "server/**/*.lua",
 })
 
 shared_scripts({
-	"config.lua",
+  "config.lua",
 })
 
 client_scripts({
-	"client/**/*.lua",
+  "client/**/*.lua",
 })
